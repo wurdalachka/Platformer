5eab8552fee28f810e1fc9f89fd8000a588b7370
wurdalachka <3840464@gmail.com>
2026-03-16 10:27:49 +0100

    Initial commit

---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMotor : MonoBehaviour
+{
+    Vector2 direction;
+    float speed = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+    }
+    void OnMove(InputValue value)
+    {
+        //Debug.Log("Move");
+        //Debug.Log(value.Get<Vector2>());
+        direction = value.Get<Vector2>();
+       
+    }
+}
+
+
+
+
+
