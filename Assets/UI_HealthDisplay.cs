89650347c8cbcfcbfcf58ad0289aa41efa34fc30
wurdalachka <3840464@gmail.com>
2026-04-16 10:56:16 +0200

    UI

--- Assets/UI_HealthDisplay.cs
+++ Assets/UI_HealthDisplay.cs
@@ -1,18 +1,27 @@
 using System;
 using UnityEngine;
+using TMPro;
 
 public class UI_HealthDisplay : MonoBehaviour
 {
     public healthcomponent healthComponent;
+    public TextMeshProUGUI textComponent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         healthComponent.OnHealthChanged += OnHealthChanged;
-        
+        healthComponent.OnHealthIntialized += OnHealthChanged;
+     
+    }
+
+    private void OnHealthChanged(float newHealth)
+    {
+       textComponent.text = newHealth.ToString();
     }
 
     private void OnHealthChanged(float newHealth, float amountChanged)
     {
-       Debug.Log(newHealth + ":" + amountChanged);
+        //Debug.Log(newHealth + ":" + amountChanged);
+        textComponent.text = newHealth.ToString();
     }
 }
--- Assets/healthcomponent.cs
+++ Assets/healthcomponent.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class healthcomponent : MonoBehaviour
@@ -5,15 +6,21 @@ public class healthcomponent : MonoBehaviour
     private float health = 10;
 
     public float maxHealth = 10;
+    private bool invicibility;
 
-
+    public delegate void OnHealthIntializedHandler(float newHealth);
     public delegate void OnHealthChangeHandler(float newHealth, float amountChanged);
+    public event OnHealthIntializedHandler OnHealthIntialized;
     public event OnHealthChangeHandler OnHealthChanged;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+        health = maxHealth;
+        OnHealthIntialized?.Invoke(health);
+
     }
 
     // Update is called once per frame
@@ -23,16 +30,31 @@ public class healthcomponent : MonoBehaviour
 
     public void AddDamage(float damage)
     {
-        health -= damage;
-        //Debug.Log(health);
+        if (!invicibility)
+        {
+            health -= damage;
+            OnHealthIntialized?.Invoke(health);
+            invicibility = true;
+            StartCoroutine(ResetInvincibility(3));
+        }
 
-        if (health <= 0)
+
+
+            if (health <= 0)
         {
             Destroy(this.gameObject);
         }
+        OnHealthIntialized?.Invoke(health);
     }
+    IEnumerator ResetInvincibility(float resetTime)
+        {
+            yield return new WaitForSeconds(resetTime);
+            Debug.Log("Reset");
+        }
 
-    public void AddHealth(float healingValue)
+
+
+public void AddHealth(float healingValue)
     {
         health += healingValue;
         OnHealthChanged?.Invoke(maxHealth, health);
@@ -40,7 +62,10 @@ public class healthcomponent : MonoBehaviour
         if (health >= maxHealth)
         {
             health = maxHealth;
+            
         }
+
+
         //Debug.Log(health);
     }
 }
