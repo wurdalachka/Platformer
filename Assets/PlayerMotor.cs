ca76dd5b7b6a1bc1cc7218ebb0dc72ed61ad6ff6
wurdalachka <3840464@gmail.com>
2026-06-11 13:42:31 +0200

    animations

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -1,136 +1,106 @@
 using System.Collections;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.PlayerLoop;
 
 public class playermotor : MonoBehaviour
 {
     Vector2 direction;
     Rigidbody2D rigidbody2d;
+    Animator animator;
+    private float lastDirection = 1f;
     public float speed = 10;
     public float jumpforce = 5;
-    public float dashforce = 15;
     public float maxspeed = 5;
     public float stoppingforce = 7;
-    private bool canJump = true;
+    public float dashForce = 20;
     private bool canDash = true;
-    private Animator animator;
-    private float initScale;
+    public LayerMask groundLayer;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        initScale = transform.localScale.x;
     }
-    
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
-        animator.SetFloat("yVelocity", rigidbody2d.linearVelocityY);
-        animator.SetFloat("xVelocity", Mathf.Abs(rigidbody2d.linearVelocityX));
-        //(check if moving right)
-       if (direction.x > 0)
-       {
-    transform.localScale = new Vector3(-initScale, transform.localScale.y, transform.localScale.z);
-       }
-       else if (direction.x < 0)
-       {
-    transform.localScale = new Vector3(initScale, transform.localScale.y, transform.localScale.z);
-       }
         HandlePlayerMovement();
         MaxSpeedLimiting();
+        UpdateAnimations();
     }
+
+    private bool IsGrounded()
+    {
+    return Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 0.8f), 0.5f);
+    }
+
+    private void UpdateAnimations()
+    {
+        animator.SetFloat("xVelocity", Mathf.Abs(rigidbody2d.linearVelocityX));
+        animator.SetFloat("yVelocity", rigidbody2d.linearVelocityY);
+        animator.SetBool("isJumping", !IsGrounded());
+
+        if (direction.x != 0)
+            lastDirection = -Mathf.Sign(direction.x);
+
+        transform.localScale = new Vector3(lastDirection * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+    }
+
     private void HandlePlayerMovement()
     {
         if (direction.x != 0)
         {
             rigidbody2d.AddForce(new Vector2(direction.x * speed, 0));
-
         }
         else if (rigidbody2d.linearVelocityX != 0)
         {
-            //zatrzymywanie
             rigidbody2d.AddForce(new Vector2(-rigidbody2d.linearVelocityX * stoppingforce, 0));
-            
         }
     }
+
     private void MaxSpeedLimiting()
-
     {
-        if (!canDash)
-        {
-            return;
-        }
-        if (rigidbody2d.linearVelocityX >= maxspeed)
-        {
-            rigidbody2d.linearVelocityX = maxspeed;
-        }
+        if (!canDash) return;
 
+        if (rigidbody2d.linearVelocityX >= maxspeed)
+            rigidbody2d.linearVelocityX = maxspeed;
         else if (rigidbody2d.linearVelocityX <= -maxspeed)
-        {
             rigidbody2d.linearVelocityX = -maxspeed;
-        }
-        //transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
     }
+
     void OnMove(InputValue value)
     {
-        // Debug.Log("Move");
-        //Debug.Log(value.Get<Vector2>());
         direction = value.Get<Vector2>();
     }
 
-    private void OnJump()
-    {
-    if (canJump)
-    {
-        rigidbody2d.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
-        animator.SetBool("isJumping", true);
-        canJump = false;
-    }
-    }
     private void OnDash()
     {
         if (canDash)
         {
             if (direction.x != 0)
-            {
-
-
-                rigidbody2d.AddForce(new Vector2(direction.x * dashforce, 0), ForceMode2D.Impulse);
-                animator.SetBool("IsDashing", true);
-
-            }
+                rigidbody2d.AddForce(new Vector2(direction.x * dashForce, 0), ForceMode2D.Impulse);
             else
-            {
-                rigidbody2d.AddForce(new Vector2(dashforce, 0), ForceMode2D.Impulse);
-                animator.SetBool("IsDashing", true);
-            }
+                rigidbody2d.AddForce(new Vector2(dashForce, 0), ForceMode2D.Impulse);
+
             canDash = false;
-            
             StartCoroutine(ResetDash(1));
         }
     }
+
     IEnumerator ResetDash(float cooldown)
     {
-
-        yield return new WaitForSeconds(cooldown);      
+        yield return new WaitForSeconds(cooldown);
         canDash = true;
-        animator.SetBool("IsDashing", false);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnJump()
     {
-        canJump = true;
-        animator.SetBool("isJumping", false);
-        
+        if (IsGrounded())
+        {
+            rigidbody2d.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);
+        }
     }
 
-     
 }
 
 
